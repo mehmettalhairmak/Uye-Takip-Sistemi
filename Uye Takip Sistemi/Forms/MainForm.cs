65202cfb3b55174dbac2189ed1ef8e19a1e989cc
Alperen Lermi <alperenler.5361@gmail.com>
2022-05-03 12:51:01 +0300

    veri doğrulama taslağı oluşturuldu

--- Uye Takip Sistemi/Forms/MainForm.cs
+++ Uye Takip Sistemi/Forms/MainForm.cs
@@ -22,16 +22,14 @@ namespace Uye_Takip_Sistemi
             InitializeComponent();
         }
 
-        DataTable dataTable;
         SqlCommand command;
-        SqlDataAdapter adapter;
         SqlConnection connection;
+        SqlDataReader dataReader;
 
         readonly string connectionString = "Data Source=sql.dhs.com.tr\\MSSQLSERVER2019;Initial Catalog=altinba1_alperen;User ID=altinba1_alpy;Password=Alpy1453*";
-
-        readonly string fetchDataString = "SELECT * FROM Students";
-
-
+        readonly string fetchUsers = "SELECT * FROM Students WHERE student_identity_number = @identityNumber";
+        
+       
         private string data;
 
 
@@ -65,7 +63,21 @@ namespace Uye_Takip_Sistemi
         private void displaydata(object sender, EventArgs e)
         {
             textBox2.Text = data;
-
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand(fetchUsers, connection);
+            command.Parameters.AddWithValue("@identityNumber", textBox2.Text);
+            
+            connection.Open();
+            dataReader = command.ExecuteReader();
+            if (dataReader.Read())
+            {
+                MessageBox.Show("Eşleşmesi başarılı");
+            }
+            else
+            {
+                MessageBox.Show("Veri Gönderilemedi");
+            }
+            connection.Close();
 
 
         }
